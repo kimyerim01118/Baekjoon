5dd66518e09cbd94ac557fa7264acb277f0195d9
김예림 <72791856+yr0118kim@users.noreply.github.com>
2024-02-27 02:28:46 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int length = 0;
+int visited[9] = {0,};
+int visited_big[9] = {0,};
+int ans[9];
+bool promising(int n) {
+		if (visited[n] == 1) {
+			return false;
+		}
+	return true;
+
+}
+
+void dfs(int start) {
+	if (start == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", ans[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		ans[start] = i;
+		if (promising(i) && ans[start-1] < i) {
+			visited[i] = 1;
+			dfs(start + 1);
+			visited[i] = 0;
+		}
+	}
+
+}
+
+int main() {
+
+	cin >> n >> m;
+
+	dfs(0);
+
+
+}
